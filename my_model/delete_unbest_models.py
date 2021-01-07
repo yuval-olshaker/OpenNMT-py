db3aae4496ce219d0257b5564beaea3523d565a4
yuvalol <yuvalolshaker@gmail.com>
2021-01-07 16:17:20 +0200

    delete models better

--- my_model/delete_unbest_models.py
+++ my_model/delete_unbest_models.py
@@ -4,10 +4,10 @@ import sys
 if len(sys.argv) < 3:
     exit(1)
 
-exp_num = sys.argv[1]
+exp_name = sys.argv[1]
 best_model_num = sys.argv[2]
 
-base_path = '/home/ubuntu/wasm_decompiler/Codenator/try_' + exp_num + '/models/'
+base_path = '/home/ubuntu/wasm_decompiler/Codenator/' + exp_name + '/models/'
 saved_models = os.listdir(base_path)
 for model in saved_models:
     if best_model_num not in model:
